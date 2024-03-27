027af9e5f0deac28198ae4197e8dfee823c78c63
annam <annam@plot-pc.cgcad.uc.edu>
2024-03-27 19:30:59 -0400

    final commit

--- Group3_Assignment08/mainPackage/main.py
+++ Group3_Assignment08/mainPackage/main.py
@@ -4,16 +4,15 @@
 # Due Date: March 28, 2024
 # Course/Section: IS 4010 - 001
 # Semester/Year: Spring 2024
-# Brief Description of the assignment: 
+# Brief Description of the assignment: connecting a database to Eclipse and joining tables in Eclipse too 
 # Citations:
-# Anything else that's relevant:
+# Anything else that's relevant: yes there are 3 different coupon for the same product so it populated all 3 coupon into sentences
 
 from tableConnectionPackage.tableConnection import *
-from functionPackage.function import *
+
 
 for row in cursor:
-    print(row)
-    
-print("The maximum quantity that I am able to purchase of the product" + str(row.ProductID) + "by the brand" + str(row.BrandID)+ "is" + str(row.MaxQtyToPurchase))
-
-
+    BrandID = row[1] 
+    ProductID= row[0]
+    MaxQtyToPurchase= row[2]
+    print("The maximum quantity that I am able to purchase of the product " + str(ProductID) + " by the brand " + str(BrandID)+ " is " + str(MaxQtyToPurchase))
